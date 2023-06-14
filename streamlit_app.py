e69251b147296580e08aa26e121aca15015fdeea
aditya singh <53287778+AdrienAlter@users.noreply.github.com>
2023-06-14 15:28:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
-streamlit.title('🥣 My parents new healthy dinner');
-streamlit.header('🥗 Breakfast Menu')
-streamlit.text('🐔 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥑 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🍞 Hard-Boiled Free-Range Egg')
+streamlit.title(' My parents new healthy dinner');
+streamlit.header('🥣 Breakfast Menu')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
+
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
